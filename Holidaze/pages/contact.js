5698c679c0ad4bb39c2cc1342c0ed153b6d7d396
doffa90 <kristoffer_skarsvaag@hotmail.com>
2022-05-01 01:56:08 +0200

    commit

--- Holidaze/pages/contact.js
+++ Holidaze/pages/contact.js
@@ -4,16 +4,23 @@ import Layout from '../components/layout/layout';
 
 
 export default function Contact() {
-    return  (
-      <div>
-        <Head>
-          <title>Holidaze | Book Now!</title>
-          <meta name='description' content='Book your Bergen adventure today! Hotels, Guesthouses and B&Bs. Booking accommodations has never been easier!'/>
-          <meta name='keywords' content='bergen, hotels, rooms, B&Bs, bookings, easy, lastminute, travel'/>
-        </Head>
-      <Layout>
+    return ( <
+        div >
+        <
+        Head >
+        <
+        title > Holidaze | Book Now! < /title> <
+        meta name = 'description'
+        content = 'Book your Bergen adventure today! Hotels, Guesthouses and B&Bs. Booking accommodations has never been easier!' / >
+        <
+        meta name = 'keywords'
+        content = 'bergen, hotels, rooms, B&Bs, bookings, easy, lastminute, travel' / >
+        <
+        /Head> <
+        Layout >
 
-      </Layout>
-  </div>
-  );
+        <
+        /Layout> <
+        /div>
+    );
 }
